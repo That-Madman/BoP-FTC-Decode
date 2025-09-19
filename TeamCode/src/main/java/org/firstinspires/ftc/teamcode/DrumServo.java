ba5a225016df7c5a3ba0a6b50547bb133e5a3163
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-09-19 16:29:14 -0400

    Removed dead class, added comments to code, fixed issues. Overall code review.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrumServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrumServo.java
@@ -9,14 +9,15 @@ import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
 public class DrumServo {
     Servo drum;
 
-    byte drumSpot = 0;
+    byte drumSpot;
+    boolean bHeld;
 
     public DrumServo(HardwareMap hardwareMap){
         drum = hardwareMap.get(Servo.class, "drum servo");
         drum.setPosition(0);
     }
     public void drumSpin(Gamepad gamepad1){
-        if (gamepad1.b) {
+        if (gamepad1.b && !bHeld) {
             ++drumSpot;
             drumSpot %= 3;
         }
@@ -37,5 +38,7 @@ public class DrumServo {
             default:
                 drumSpin(gamepad1); //If it fails, we just try one more time to let modulo work
         }
+
+        bHeld = gamepad1.b;
     }
 }
