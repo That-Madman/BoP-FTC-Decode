51eb1bc64005f47a8a7f26ccc0b0ec91f4258f22
Zamboniman187 <jtcrensh@gmail.com>
2025-10-11 10:13:38 -0400

    new temporary auto class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoTemporary.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.teleops.DriveBase;
+
+@Autonomous
+public class AutoTemporary extends LinearOpMode {
+    DriveBase driveBase;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driveBase = new DriveBase(hardwareMap);
+        waitForStart();
+        sleep(2000);
+        driveBase.setPowers(1,0,0);
+        sleep(500);
+        driveBase.setPowers(0,0,0);
+    }
+}
