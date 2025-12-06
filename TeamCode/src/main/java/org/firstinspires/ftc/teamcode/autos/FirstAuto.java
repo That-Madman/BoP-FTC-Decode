44f3c10691dc20a6b0baf832e9ebf728c88f771d
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-12-06 10:20:25 -0500

    Going further

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/FirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/FirstAuto.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.autos;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -28,6 +27,8 @@ public class FirstAuto extends LinearOpMode {
                 .action(() -> {
                     telemetry.addLine("Shooting 1...");
                     telemetry.update();
+
+                    sleep(1000);
                 })
                 .headingConstant(90)
                 .point(new Vector2D(12, 24))
@@ -35,13 +36,15 @@ public class FirstAuto extends LinearOpMode {
                     telemetry.addLine("Intaking...");
                     telemetry.update();
                 })
-                .point(new Vector2D(12, 48))
+                .point(new Vector2D(12, 60))
                 .point(new Vector2D(12, 24))
                 .headingConstant(45)
                 .point(new Vector2D(24, 24))
                 .action(() -> {
                     telemetry.addLine("Shooting 2...");
                     telemetry.update();
+
+                    sleep(1000);
                 })
                 .build();
 
