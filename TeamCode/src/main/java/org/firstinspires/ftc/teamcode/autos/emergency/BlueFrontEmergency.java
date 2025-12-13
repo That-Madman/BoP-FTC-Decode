e1bfa99079bf160ad56b2684c31f980cb4a2010c
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-12-13 12:44:30 -0500

    These needed to be given start pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/emergency/BlueFrontEmergency.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/emergency/BlueFrontEmergency.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode.autos.emergency;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.trailblazer.drivebase.Drive;
 import org.firstinspires.ftc.teamcode.trailblazer.path.Path;
 import org.fotmrobotics.trailblazer.Vector2D;
+
 @Autonomous(group = "Emergency")
 public class BlueFrontEmergency extends LinearOpMode{
     Drive drive;
@@ -20,6 +23,8 @@ public class BlueFrontEmergency extends LinearOpMode{
                 .build();
 
         waitForStart();
+
+        drive.odometry.setPosition(-72, 24, 0, DistanceUnit.INCH, AngleUnit.DEGREES);
 
         p.run(telemetry);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/emergency/RedFrontEmergency.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/emergency/RedFrontEmergency.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode.autos.emergency;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.trailblazer.drivebase.Drive;
 import org.firstinspires.ftc.teamcode.trailblazer.path.Path;
 import org.fotmrobotics.trailblazer.Vector2D;
+
 @Autonomous(group = "Emergency")
 public class RedFrontEmergency extends LinearOpMode {
     Drive drive;
@@ -22,6 +25,8 @@ public class RedFrontEmergency extends LinearOpMode {
         waitForStart();
 
         p.run(telemetry);
+
+        drive.odometry.setPosition(-72, -24, 0, DistanceUnit.INCH, AngleUnit.DEGREES);
 
         drive.runMotors(new double[4]);
     }
