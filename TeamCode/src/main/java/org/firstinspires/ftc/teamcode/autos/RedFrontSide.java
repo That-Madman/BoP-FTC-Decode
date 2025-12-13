0fb64403e4637341da4cad82e8aa31286cd8e9dc
Zamboniman187 <jtcrensh@gmail.com>
2025-12-13 10:19:41 -0500

    Emergency Front Start Autos added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedFrontSide.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.trailblazer.drivebase.Drive;
+import org.firstinspires.ftc.teamcode.trailblazer.path.Path;
+import org.fotmrobotics.trailblazer.Vector2D;
+@Autonomous
+public class RedFrontSide extends LinearOpMode {
+    Drive drive;
+    Path p;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drive = new Drive(hardwareMap);
+        waitForStart();
+        p = drive.PathBuilder(new Vector2D(-72, -24))
+                .point(new Vector2D(-60, -6))
+                .point(new Vector2D(-48, -24))
+                .build();
+    }
+}
