5bbaaf164d8e7f2af7af5452854bd82d5a1ab952
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-12-27 10:23:24 -0500

    Let's begin behavior trees!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BehaviorTrees/Actions/firePatterns/UnpatternedFire.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.BehaviorTrees.Actions.firePatterns;
+
+import com.ftcteams.behaviortrees.DebugTree;
+
+import org.firstinspires.ftc.teamcode.BehaviorTrees.BNode;
+import org.firstinspires.ftc.teamcode.BehaviorTrees.boPMode.BoPMode;
+
+public class UnpatternedFire extends BNode {
+    @Override
+    public State tick(DebugTree debug, BoPMode opMode) {
+        return null;
+    }
+}
