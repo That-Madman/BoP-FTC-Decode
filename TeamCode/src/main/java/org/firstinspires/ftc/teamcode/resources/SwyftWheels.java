f87ddb46e8ff3c403a8035ab13ee1df912e6ae35
Zamboniman187 <jtcrensh@gmail.com>
2026-01-10 11:02:05 -0500

    Now added a color sensing feature to the swyft wheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/SwyftWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/SwyftWheels.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.resources;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -7,14 +8,24 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class SwyftWheels {
     private final DcMotor sWheel;
+    private final ColorSensor color;
     public SwyftWheels(HardwareMap hardwareMap) {
         sWheel = hardwareMap.get(DcMotor.class, "swyftWheels");
         sWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        color = hardwareMap.get(ColorSensor.class,"color sense");
     }
     public void WheelSpin(Gamepad gamepad2, Telemetry telemetry) {
         sWheel.setPower((gamepad2.y ? 1 : 0) - (gamepad2.x ? 1 : 0));
     }
     public void autoSpin(double power){
         sWheel.setPower(power);
+    }
+    public boolean ballSense(Telemetry telemetry){
+        if((color.blue() >= 170 && color.blue() <= 210) ||
+        (color.green() >= 160 && color.green() <= 200)){
+            return true;
+        }
+        else
+            return false;
     }
 }
