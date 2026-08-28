bd3b87ea25ca4643e66bc3cab27dfc40a5ac5fa9
Zamboniman187 <jtcrensh@gmail.com>
2026-08-28 17:50:40 -0400

    Updating for Training

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/exampleMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/exampleMotor.java
@@ -17,10 +17,10 @@ public class exampleMotor extends OpMode {
         fLeft = hardwareMap.get(DcMotor.class, "fLeft");
         fLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fLeft.setPower(0);
-        bLeft = hardwareMap.get(DcMotor.class, "fLeft");
+        bLeft = hardwareMap.get(DcMotor.class, "bLeft");
         bLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bLeft.setPower(0);
-        bRight = hardwareMap.get(DcMotor.class, "fLeft" );
+        bRight = hardwareMap.get(DcMotor.class, "bRight" );
         bRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bRight.setPower(0);
         fRight = hardwareMap.get(DcMotor.class, "fRight");
@@ -35,8 +35,8 @@ public class exampleMotor extends OpMode {
         double y = -gamepad1.left_stick_y;
         double r = gamepad1.right_stick_x;
         fRight.setPower(y-x-r);
-        fLeft.setPower(y+x-r);
+        fLeft.setPower(y+x+r);
         bLeft.setPower(y-x+r);
-        bRight.setPower(y+x+r);
+        bRight.setPower(y+x-r);
     }
 }
